5775f085dfe01651848f46e2f6a5c883a77385d9
ilgar34 <ilgar34@gmail.com>
2024-10-03 01:50:36 +0300

    homework 02

---
+++ week/week01/homework02/Homework 2/Program.cs
@@ -0,0 +1,27 @@
+﻿namespace homework_deneme_proje02
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Hamza'nın özellikleri
+            string catName = "Hamza";
+            int catAge = 8;
+            string catBreed = "Tekir";
+            string catGender = "Erkek";
+            int livingWithOwnerSince = 2019;
+            string favoriteActivies = "evde koşmak ve oyuncağı Zuzu ile oynamak";
+
+            Console.WriteLine($"{catName}  {catAge} yaşında bir kedidir.");
+            Console.WriteLine($"Cinsi {catBreed} {catGender} bir kedidir.");
+            Console.WriteLine($"{livingWithOwnerSince} yılından beri benimle yaşamaktadır.");
+            Console.WriteLine($"{catName}'nın çok sevdiği şeyler: {favoriteActivies}.");
+
+
+
+
+
+
+        }
+    }
+}
